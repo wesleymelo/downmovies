1be2a494cec6443d5965f104cffd62e3b505ae7a
sneck07 <sneck07@gmail.com>
2012-10-16 05:49:11 +0000

    Criação de usuario

---
+++ downmovies/src/br/ucb/filmes/beans/Usuario.java
@@ -0,0 +1,84 @@
+package br.ucb.filmes.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+public class Usuario implements Serializable{
+	@Id
+	@GeneratedValue
+	private Integer id_usuario;
+	
+	@Column
+	private String nome;
+	
+	@Column	
+	private Categoria sobrenome;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String senha;
+	
+	@Column
+	private Integer id_perfil;
+
+	public Integer getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(Integer id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(Categoria sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Integer getId_perfil() {
+		return id_perfil;
+	}
+
+	public void setId_perfil(Integer id_perfil) {
+		this.id_perfil = id_perfil;
+	}
+
+	
+	
+}
---
+++ downmovies/src/br/ucb/filmes/managedBean/CadastroUsuarioManagedBean.java
@@ -0,0 +1,63 @@
+package br.ucb.filmes.managedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+
+@ManagedBean
+@ViewScoped
+public class CadastroUsuarioManagedBean  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String nomeUsuario;
+	private String sobrenome;
+	private String email;
+	private String senha;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
