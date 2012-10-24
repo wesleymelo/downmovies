91c68ffce514a8467eebca2e74fa0264dc5b4584
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@2278cab0-b8f1-5bc1-df82-44ae82d4c413>
2012-10-24 20:37:24 +0000

    CategoriaConverter

---
+++ trunk/downmovies/src/br/ucb/filmes/converter/CategoriaConverter.java
@@ -0,0 +1,32 @@
+package br.ucb.filmes.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+import br.ucb.filmes.beans.Categoria;
+import br.ucb.filmes.dao.CategoriaDAO;
+
+@FacesConverter(value="CategoriaConverter", forClass=Categoria.class)
+public class CategoriaConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		if(value != null && !value.isEmpty()) {  
+			System.out.println("Valor: "+Integer.parseInt(value));
+            return new CategoriaDAO().consult(Integer.parseInt(value)); 
+        }  
+        return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object object) {
+		if(object != null && object instanceof Categoria) {  
+			System.out.println(((Categoria)object));
+            return ((Categoria)object).getIdCategoria().toString();  
+        }  
+        return null;  
+	}
+
+}
