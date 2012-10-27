10b3bb59282c6d7a08ffe0f789c7387706604d5d
sneck07@gmail.com <sneck07@gmail.com@2278cab0-b8f1-5bc1-df82-44ae82d4c413>
2012-10-27 14:59:23 +0000

    Não altera nada q eu fiz senão eu te mato

--- trunk/downmovies/src/br/ucb/filmes/managedBean/FilmeManagedBean.java
+++ trunk/downmovies/src/br/ucb/filmes/managedBean/FilmeManagedBean.java
@@ -1,40 +1,40 @@
 package br.ucb.filmes.managedBean;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
-import javax.faces.application.FacesMessage;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import org.primefaces.event.FileUploadEvent;
+import javax.faces.bean.ViewScoped;
+
 import org.primefaces.model.UploadedFile;
+
 import br.ucb.filmes.beans.Filme;
 import br.ucb.filmes.dao.FilmeDAO;
 import br.ucb.filmes.enums.EnumFormato;
 import br.ucb.filmes.enums.EnumQualidade;
+import br.ucb.filmes.upload.UploadArquivo;
 import br.ucb.filmes.util.FacesUtil;
 
 @ManagedBean
+@ViewScoped
 public class FilmeManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+
 	private Filme filme;
 	private List<Filme> filmes;
-	private UploadedFile file;
-	
-	public FilmeManagedBean() {
+	private UploadArquivo uploadArquivo;
+	private UploadedFile arqFilme; 
+	private UploadedFile arqImagem; 
+ 	public FilmeManagedBean() {
+ 	
 		filme = new Filme();
+		uploadArquivo = new UploadArquivo();
 	}
 	
 
-	public void upload(FileUploadEvent event){
-		file = event.getFile();
-		
-	}
 	
 	public Map<String, Integer> getMapFormato() {
 		return EnumFormato.getMapaFomato();
@@ -47,14 +47,19 @@ public class FilmeManagedBean implements Serializable {
 	
 	
 	public void cadastraFilme()	{
+
 		try {
-			System.out.println("Qualidade:"+filme.getQualidade());
-			System.out.println("Formato:"+filme.getFormato());
+
 			
 			
 			FilmeDAO insert = new FilmeDAO();
 			
 			insert.insert(filme);
+			uploadArquivo.fileUploadActionTorrent(arqFilme);
+			uploadArquivo.fileUploadActionImagem(arqImagem);
+			uploadArquivo.gravarFilme();
+			uploadArquivo.gravarImagem();
+			
 			FacesUtil.mensInfo("Filme cadastrado com Sucesso");
 			
 		} catch (Exception e) {
@@ -63,140 +68,46 @@ public class FilmeManagedBean implements Serializable {
 		}
 		
 	}
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
+	public UploadedFile getArqFilme() {
+		return arqFilme;
+	}
+
+
+
+	public void setArqFilme(UploadedFile arqFilme) {
+		this.arqFilme = arqFilme;
+	}
+
+
+
+	public UploadedFile getArqImagem() {
+		return arqImagem;
+	}
+
+
+
+	public void setArqImagem(UploadedFile arqImagem) {
+		this.arqImagem = arqImagem;
+	}
+
+
 
 	public List<Filme> getFilmes() {
 		return filmes;
 	}
-
+	
 	public void setFilmes(List<Filme> filmes) {
 		this.filmes = filmes;
 	}
-
+	
 	public Filme getFilme() {
 		return filme;
 	}
-
+	
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}	
-	
-	public UploadedFile getFile() {
-		return file;
-	}
-
-	public void setFile(UploadedFile file) {
-		this.file = file;
-	}
-		
-	public String cadastrar(){
-		return "filmeForm";
-	}
-	public void fileUploadActionImagem(FileUploadEvent event) {
-		
-	}
-	
-	public void fileUploadActionTorrent(FileUploadEvent event) {
-		
-        try {
-            
-            String caminho = FacesContext.getCurrentInstance().getExternalContext()
-					.getRealPath("\\filmes\\");
-            
-            
-            
- 
- // Aqui cria o diretorio caso não exista
-            File file = new File(caminho);
-            file.mkdirs();
-            
-           
-    		
-    		System.out.println(event.getFile().getFileName());
-    		
-    		
-    		System.out.println("Caminho: "+caminho);
-            
-            
-            byte[] arquivo = event.getFile().getContents();
-            
-            
-            System.out.println("arq"+arquivo);
-            caminho = caminho +"\\"+event.getFile().getFileName();    
-      
-            
-            System.out.println("Caminho: "+caminho);
- // esse trecho grava o arquivo no diretório
-            FileOutputStream fos = new FileOutputStream(caminho);
-            fos.write(arquivo);
-            fos.close();
-            
-            FacesUtil.mensInfo("O arquivo "+event.getFile().getFileName() + " foi carregado com Sucesso");
-            
-        } catch (Exception ex) {
-            System.out.println("Erro no upload de imagem" + ex);
-        }
-        
-
-    }
-	
-	
-	
-	
-	
-	public String uploadFilme(){
-		
-		
-		System.out.println("hhhhhhhcassete!");
-		
-		System.out.println(file);
-		
-		if(getFile() != null){
-			FacesMessage msg = new FacesMessage("Sucesso", getFile().getFileName() + " foi carregado.");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			
-			
-			String caminho = FacesContext.getCurrentInstance().getExternalContext()
-					.getRealPath("" + "\\filmes\\" + getFile().getFileName());
-			
-			
-			
-			System.out.println(getFile().getFileName());
-			
-			System.out.println(caminho);
-			
-			byte[] conteudo = getFile().getContents();
-			FileOutputStream fos;
-			try {
-				fos = new FileOutputStream(caminho);
-				fos.write(conteudo);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-			return "filmeList";
-			
-		}
-		
-		System.out.println("Ah nem!");
-		
-		return "filmeList";
-		
-
-	}
 	
 }
