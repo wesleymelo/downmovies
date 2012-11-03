3d33b3c89127fd83d7783dd1e6999e62600f7610
wesley.melo.pereira <wesley.melo.pereira@gmail.com>
2012-11-03 13:53:25 +0000

    DAO Generic

--- downmovies/src/br/ucb/filmes/dao/FilmeDAO.java
+++ downmovies/src/br/ucb/filmes/dao/FilmeDAO.java
@@ -1,48 +1,10 @@
 package br.ucb.filmes.dao;
 
-import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.exception.ConstraintViolationException;
 import br.ucb.filmes.beans.Filme;
 
 
-public class FilmeDAO {
-	public void insert(Filme filme) {
-		Session session = HibernateUtil.getSession();
-		Transaction transaction = session.beginTransaction();
-		session.saveOrUpdate(filme);
-		transaction.commit();
-		session.close();
-	}
+public class FilmeDAO extends GenereicDAO<Filme> {
 
-	public void delete(Filme filme) throws ConstraintViolationException {
-		Session session = HibernateUtil.getSession();
-		Transaction transaction = session.beginTransaction();
-		session.delete(filme);
-		transaction.commit();
-		session.close();
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<Filme> recoveryAll() {
-		Session session = HibernateUtil.getSession();
-		try {
-			return session.createCriteria(Filme.class).list();
-		} finally {
-			session.close();
-		}
-	}
-	public Filme consult(long id) {
-		Session session = HibernateUtil.getSession();
-		try {
-			Filme filme = (Filme) session.get(Filme.class, id);
-			return filme;
-		} finally {
-			session.close();
-		}
-	}
-
-
+	private static final long serialVersionUID = 1L;
+	
 }
--- downmovies/src/br/ucb/filmes/dao/UserDAO.java
+++ downmovies/src/br/ucb/filmes/dao/UserDAO.java
@@ -1,47 +1,11 @@
 package br.ucb.filmes.dao;
 
-import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.hibernate.exception.ConstraintViolationException;
 import br.ucb.filmes.beans.Usuario;
 
-public class UserDAO {
-	public void insert(Usuario usuario) {
-		Session session = HibernateUtil.getSession();
-		Transaction transaction = session.beginTransaction();
-		session.saveOrUpdate(usuario);
-		transaction.commit();
-		session.close();
-	}
+public class UserDAO extends GenereicDAO<Usuario> {
 
-	public void delete(Usuario usuario) throws ConstraintViolationException {
-		Session session = HibernateUtil.getSession();
-		Transaction transaction = session.beginTransaction();
-		session.delete(usuario);
-		transaction.commit();
-		session.close();
-	}
-
-	@SuppressWarnings("unchecked")
-	public List<Usuario> recoveryAll() {
-		Session session = HibernateUtil.getSession();
-		try {
-			return session.createCriteria(Usuario.class).list();
-		} finally {
-			session.close();
-		}
-	}
-	public Usuario consult(int id) {
-		Session session = HibernateUtil.getSession();
-		try {
-			Usuario usuario = (Usuario) session.get(Usuario.class, id);
-			return usuario;
-		} finally {
-			session.close();
-		}
-	}
-
+	private static final long serialVersionUID = 1L;
+	
 }
 
 
