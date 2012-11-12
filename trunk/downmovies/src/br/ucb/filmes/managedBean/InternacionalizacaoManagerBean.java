1dec6dc7c4acd326ce0d2d5f3960f4858c886016
wesley.melo.pereira@gmail.com <wesley.melo.pereira@gmail.com@2278cab0-b8f1-5bc1-df82-44ae82d4c413>
2012-11-12 04:54:44 +0000

    Internacionalização

---
+++ trunk/downmovies/src/br/ucb/filmes/managedBean/InternacionalizacaoManagerBean.java
@@ -0,0 +1,39 @@
+package br.ucb.filmes.managedBean;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class InternacionalizacaoManagerBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	private Locale currentLocale = new Locale("pt", "BR");  
+
+	public void englishLocale() {  
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();  
+		currentLocale = Locale.US;  
+		viewRoot.setLocale(currentLocale); 
+	}  
+
+	public void portugueseLocale() {  
+		UIViewRoot viewRoot = FacesContext.getCurrentInstance().getViewRoot();  
+		currentLocale = new Locale("pt", "BR");  
+		viewRoot.setLocale(currentLocale); 
+	}  
+
+	public Locale getCurrentLocale() {  
+		return currentLocale;  
+	}  
+
+
+}
